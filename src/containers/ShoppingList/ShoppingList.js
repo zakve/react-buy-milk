065e511a2a5c8ba554ab86d93531279911d6b83e
zakve <zaklas.martin@gmail.com>
2021-03-14 19:58:03 +0100

    Add material-ui and material-ui styles

--- src/containers/ShoppingList/ShoppingList.js
+++ src/containers/ShoppingList/ShoppingList.js
@@ -1,7 +1,23 @@
+import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
+});
+
 const ShoppingList = () => {
+    const classes = useStyles();
     return (
         <>
-            <div>List</div>
+            <div className={classes.root}>List</div>
             <div>Controls</div>
         </>
     )
